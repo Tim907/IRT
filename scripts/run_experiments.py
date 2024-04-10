c09443f3f33268d4033a20a60731ed3de7a25d2a
akrivosija <34035849+akrivosija@users.noreply.github.com>
2024-04-10 20:59:59 +0200

    Cleared code

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -5,10 +5,10 @@ from IRT.datasets import BasicDataset, RealisticDataset, Realistic3PLDataset
 from IRT.utils import run_experiments
 
 #SIZES = (0, 500)
-NUM_RUNS = 1
-THREE_PL = False #True
+NUM_RUNS = 1 # Sets how many repetitions of the same experiment 
+THREE_PL = False #True  #Sets whether it is 2PL or 3PL experiment
 
-dataset = RealisticDataset(use_caching=False)
+dataset = RealisticDataset(use_caching=False) # Sets which input set of labels is used
 #dataset = Realistic3PLDataset(use_caching=False)
 
 #dataset = BasicDataset(100000,200,use_caching=False)
@@ -25,7 +25,7 @@ dataset = RealisticDataset(use_caching=False)
 run_experiments(
     dataset=dataset,
     num_runs=NUM_RUNS,
-    sizes=(0, 50),
+    sizes=(0, 50), # Sets the coreset sizes for (items, examinees). "0" - no coreset computed
     add=True,
     ThreePL=THREE_PL
 )
@@ -38,42 +38,3 @@ run_experiments(
     # ThreePL=THREE_PL
 # )
 
-# run_experiments(
-    # dataset=dataset,
-    # num_runs=NUM_RUNS,
-    # sizes=(0, 4000),
-    # add=True,
-    # ThreePL=THREE_PL
-# )
-
-# run_experiments(
-    # dataset=dataset,
-    # num_runs=NUM_RUNS,
-    # sizes=(0, 6000),
-    # add=True,
-    # ThreePL=THREE_PL
-# )
-
-# run_experiments(
-    # dataset=dataset,
-    # num_runs=NUM_RUNS,
-    # sizes=(0, 8000),
-    # add=True,
-    # ThreePL=THREE_PL
-# )
-
-# run_experiments(
-    # dataset=dataset,
-    # num_runs=NUM_RUNS,
-    # sizes=(0, 10000),
-    # add=True,
-    # ThreePL=THREE_PL
-# )
-
-# run_experiments(
-    # dataset=dataset,
-    # num_runs=NUM_RUNS,
-    # sizes=(0, 20000),
-    # add=True,
-    # ThreePL=THREE_PL
-# )
